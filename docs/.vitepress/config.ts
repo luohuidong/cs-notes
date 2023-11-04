5476e8dae619c3368cf943b0148f6ef3f12a1363
luohuidong <luohuidong01@126.com>
2023-11-04 23:56:59 +0800

    notes init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Notes",
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [],
+
+    sidebar: [
+      {
+        text: "Web Components",
+        items: [
+          { text: "Custom element", link: "/web-components/custom-element" },
+          { text: "Shadow DOM", link: "/web-components/shadow-dom" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/luohuidong/website-notes" },
+    ],
+  },
+});
