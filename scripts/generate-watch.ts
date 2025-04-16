5046ffc4749e6dc79a4fbdbed12a95f35e357e4f
luohuidong <luohuidong@126.com>
2025-04-16 09:55:50 +0800

    Apr 16, 2025, 09:55 AM

--- scripts/generate-watch.ts
+++ scripts/generate-watch.ts
@@ -1,26 +1,24 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import { debounce } from 'lodash'
+import _ from 'lodash'
 import { SidebarGenerator } from './SidebarGenerator.js'
 import { NavGenerator } from './NavGenerator.js'
 
-const sidebarGenerator = new SidebarGenerator()
-const navGenerator = new NavGenerator()
-
-const debounceGenerate = debounce(async () => {
+const debounceGenerate = _.debounce(async () => {
+  const sidebarGenerator = new SidebarGenerator()
+  const navGenerator = new NavGenerator()
   await sidebarGenerator.generate()
   await navGenerator.generate()
 }, 1000)
 
 fs.watch('docs', { recursive: true }, (eventType, filename) => {
   if (!filename) return
+  if (filename.startsWith('.vitepress')) return
 
   const result = path.parse(filename)
   if (result.name === 'index' && result.ext === '.md') {
     debounceGenerate()
   }
 })
-
-sidebarGenerator.generate()
 
 console.log('watching...')
